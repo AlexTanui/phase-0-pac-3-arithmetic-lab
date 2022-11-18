1d8920e53f543c30c94cbdfca723e60c01e298f1
AlexTanui <tanuialex417@gmail.com>
2022-11-18 13:01:50 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a, b){
+   return a + b
+}
+
+function subtract(a, b){
+    return a - b
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide(a, b){
+    return a / b
+}
+
+function increment(n){
+     
+   return   ++n
+
+}
+
+function decrement(n){
+
+    return --n
+}
+
+function makeInt(n){
+     
+     return (parseInt(n,10))
+}
+// function makeInt(n){
+//     parseInt('sldkjflksjf', 10);
+//     return (n)
+// }
+
+function preserveDecimal(n){
+    
+    return (parseFloat(n))
+}
+
